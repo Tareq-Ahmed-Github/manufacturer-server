a412a1c0aae2dbe6c7169b836d7eec07a33f6da8
unknown <tareqrobin57@gmail.com>
2022-05-28 16:13:56 +0600

    mongodb connented, getting data successfully

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ian6n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db("bike-hunt").collection("tools");
+
+    app.get("/tools", async (req, res) => {
+      const tools = await toolsCollection.find({}).toArray();
+      console.log(tools);
+      res.send(tools);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('manufacturer server worked')
 })
